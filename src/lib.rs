907748be0e244d76ef48bee1f855745e4cebea21
Lucas <AgtLucas@users.noreply.github.com>
2016-10-01 19:21:43 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub static MOTIVATIONS: [&'static str; 60] = [
+pub static MOTIVATIONS: [&'static str; 61] = [
     "don't worry. no one actually knows what they're doing."
   , "this is Hard Stuff, but you can do it!"
   , "getting started is hard, you did it, congratulations!"
@@ -59,4 +59,5 @@ pub static MOTIVATIONS: [&'static str; 60] = [
   , "You're the best! You can do anything!"
   , "Alright , let's go"
   , "Don't hate, caffeinate"
+  , "Keep Pushing!"
 ];
