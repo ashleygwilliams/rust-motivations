e599a1ce04f39122ece93606d109cde66f079a63
Lucas <AgtLucas@users.noreply.github.com>
2016-10-01 19:23:39 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub static MOTIVATIONS: [&'static str; 60] = [
+pub static MOTIVATIONS: [&'static str; 61] = [
     "don't worry. no one actually knows what they're doing."
   , "this is Hard Stuff, but you can do it!"
   , "getting started is hard, you did it, congratulations!"
